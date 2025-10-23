8789bab2c771116cc4a60f69c9f7538ed9df66d5
ChoiEUH <cuh0108@naver.com>
2025-10-23 16:29:49 +0900

    Add industry_iot_form_project

---
+++ industry_iot_form/test.cs
@@ -0,0 +1,29 @@
+using System;
+using static System.Console;
+
+
+
+
+namespace Hello;
+internal class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+        int a = 1;
+
+        float b = 2.3f;
+        object c = 0;
+
+        WriteLine("여러분, 안녕하세요?");
+        WriteLine("반갑습니다");
+
+        c = a + b;
+
+        WriteLine(c);
+
+        return;
+    }
+}
